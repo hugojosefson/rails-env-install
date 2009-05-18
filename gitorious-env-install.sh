405fb2f5ab975be0db32b17547532050ab45ad58
Hugo Josefson <hugo@josefson.org>
2009-05-19 00:50:09 +0200

    Added ubuntu package ruby1.8-dev.

--- gitorious-env-install.sh
+++ gitorious-env-install.sh
@@ -11,7 +11,7 @@ echo "You need to be root to run this!"
   exit 2
 fi
 
-apt-get install -y librmagick-ruby libonig-dev libbluecloth-ruby libopenssl-ruby1.8 rubygems
+apt-get install -y librmagick-ruby libonig-dev libbluecloth-ruby libopenssl-ruby1.8 rubygems ruby1.8-dev
 
 gem install textpow mime-types --no-ri --no-rdoc
 
